7f0cb0a3f150c4d24c2abb55ec1f36e63367e0eb
Mohammed Junaid <mdjunaid6731@gmail.com>
2024-10-24 18:07:46 +0530

    Delete Chapter 10/02_instance_vs_class_att.py

--- Chapter 10/02_instance_vs_class_att.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    language = "python"  
-    salary = 1200000
-
-rohan = Employee()  
-rohan.language = "javascript"
-print( rohan.language, rohan.salary)
- #instance attr has more prefrence over class attr
